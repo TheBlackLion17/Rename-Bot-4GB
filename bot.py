4bb3da63c2524cd5e1f20b3b475bcaba71b894a1
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-07-30 19:51:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ class Bot(Client):
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username
-        self.force_channel = Config.FORCE_SUB
+        self.force_channel = FORCE_SUB
 
         # Force Subscription Invite Link
         if self.force_channel:
